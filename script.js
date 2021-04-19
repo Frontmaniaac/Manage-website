15fba192aeb646dd951d54d8a05b922ef23e8cc8
Marcin Stanik <mstanik25@gmail.com>
2021-04-19 17:14:31 +0200

    fixed slider scroll

--- script.js
+++ script.js
@@ -2,26 +2,30 @@
 const sliderItems = document.querySelectorAll(".slider__item");
 const sliderBtns = document.querySelectorAll(".sliderButtons__circle");
 const slider = document.querySelector(".slider");
-// sliderBtns.forEach((item, index) => {
-//   item.addEventListener("click", () => {
-//     sliderBtns.forEach((item, index) => {
-//       sliderBtns[index].classList.remove("activeCircle");
-//     });
-//     if (
-//       document.body.clientWidth > 480 &&
-//       sliderBtns.length == 4 &&
-//       index == 3
-//     ) {
-//       slider.style.transform =
-//         "translateX(" + (-sliderItems[index].clientWidth + 40) * index + "px)";
-//     } else {
-//       slider.style.transform =
-//         "translateX(" + -sliderItems[index].clientWidth * index + "px)";
-//     }
+if (document.body.clientWidth > 600) {
+  sliderBtns.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      sliderBtns.forEach((item, index) => {
+        sliderBtns[index].classList.remove("activeCircle");
+      });
+      if (
+        document.body.clientWidth > 480 &&
+        sliderBtns.length == 4 &&
+        index == 3
+      ) {
+        slider.style.transform =
+          "translateX(" +
+          (-sliderItems[index].clientWidth + 40) * index +
+          "px)";
+      } else {
+        slider.style.transform =
+          "translateX(" + -sliderItems[index].clientWidth * index + "px)";
+      }
 
-//     sliderBtns[index].classList.add("activeCircle");
-//   });
-// });
+      sliderBtns[index].classList.add("activeCircle");
+    });
+  });
+}
 // let index = 1;
 // if (document.body.clientWidth < 600)
 //   setInterval(() => {
